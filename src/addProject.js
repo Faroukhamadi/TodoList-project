d59b9695b0c63ff898ad1e859a31af8b05f2b335
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-01-22 01:55:12 +0100

    Fix Add project functionality

--- src/addProject.js
+++ src/addProject.js
@@ -2,6 +2,8 @@ import removeChildren from "./delChildren";
 
 
 export default function addProject() {
+    let projects = [];
+    const togglable = document.querySelectorAll('.togglable');
     const addProjectBtn = document.getElementById('addProject');
     const cancelBtn = document.createElement('button');
     const inputField = document.createElement('input');
@@ -9,6 +11,7 @@ export default function addProject() {
     const listItem1 = document.createElement('li');
     const listItem2 = document.createElement('li');
     const addBtn = document.createElement('button');
+    const taskContainer = document.querySelector('.task-container');
 
     cancelBtn.addEventListener('click', e => {
         sideBarList.appendChild(addProjectBtn);
@@ -32,31 +35,51 @@ export default function addProject() {
             checkListImg.alt = 'checklist';
             checkListImg.id = 'list';
             text.textContent = inputField.value;
+            projects.push(inputField.value);
+            console.log(projects);
             text.href = '#';
             sideBarList.appendChild(listItemN);
             listItemN.appendChild(checkListImg);
             listItemN.appendChild(text);
             sideBarList.appendChild(addProjectBtn);
-            console.log(addProjectBtn);
+            let togglable = document.querySelectorAll('.togglable');
+            for (const element of togglable) {
+                console.log(element.textContent);
+            }
+            for (let i = 0; i < projects.length; i++) {
+                console.log('This is projects[', i, ']', projects[i]);
+                togglable[i + 4].addEventListener('click', e => {
+                    removeChildren(taskContainer);
+                    const title = document.createElement('h1');
+                    const taskList = document.createElement('ul');
+                    title.innerHTML = projects[i];
+                    taskContainer.appendChild(title);
+                    taskContainer.appendChild(taskList);
+                    taskList.insertAdjacentHTML('afterbegin', `<li class="togglable"><span class="plus-sgn">+</span> Add Task</li>`);
+                })
+            }
+
 
         }
     })
 
 
     addProjectBtn.addEventListener('click', e => {
-        console.log(`I'm getting clicked.`);
-        addBtn.id = 'add-btn';
-        cancelBtn.id = 'cancel-btn';
-        inputField.placeholder = 'Project Name';
-        addBtn.textContent = 'Add';
-        cancelBtn.textContent = 'Cancel';
-        sideBarList.appendChild(listItem1);
-        listItem1.appendChild(inputField);
-        sideBarList.appendChild(listItem2);
-        listItem2.appendChild(addBtn);
-        listItem2.appendChild(cancelBtn);
-        addProjectBtn.remove();
-
+        if (projects.length >= 5) {
+            alert(`Limit Exceeded.   Edit or Delete an existing element`);
+        } else {
+            console.log(`I'm getting clicked.`);
+            addBtn.id = 'add-btn';
+            cancelBtn.id = 'cancel-btn';
+            inputField.placeholder = 'Project Name';
+            addBtn.textContent = 'Add';
+            cancelBtn.textContent = 'Cancel';
+            sideBarList.appendChild(listItem1);
+            listItem1.appendChild(inputField);
+            sideBarList.appendChild(listItem2);
+            listItem2.appendChild(addBtn);
+            listItem2.appendChild(cancelBtn);
+            addProjectBtn.remove();
+        }
     })
-
 }
